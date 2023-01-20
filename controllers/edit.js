bcb61f1f8cd75243084c065ef39fc01c12102f85
Brandon White <brandonkwhite3@gmail.com>
2023-01-20 03:48:34 -0500

    Edit and delete routing for contact page corrected. Fix button layout next. Then work on populating doctor directory from additions in meds. Develop calendar functionality, and add a tracker to the first page in the med tracker. Edit routing for contacts needs styling and adjustment.

--- controllers/edit.js
+++ controllers/edit.js
@@ -1,23 +1,26 @@
 const Contact = require('../models/Contact');
+const contacts = require('./contacts');
 
 module.exports = {
-    // remember: if you're talking to your db, pop an async to allow for the app to twait for responses.
-    getEdit : async (req,res) => {
-        const id= req.params.id
-        console.log(id)
-        try {
-            const items = await ItemList.find()
-            res.render("edit.ejs", {itemList: items, idItem: id})
-        } catch (err) {
-            if (err) return res.status(500).send(err)
-        }
-    },
+    // // remember: if you're talking to your db, pop an async to allow for the app to twait for responses.
+    // getEdit : async (req,res) => {
+    //     const id= req.params.id
+    //     console.log(id)
+    //     try {
+    //         const contacts = await Contact.find()
+    //         res.render("edit.ejs", {contacts: contacts, idContact: id})
+    //     } catch (err) {
+    //         if (err) return res.status(500).send(err)
+    //     }
+    // },
     getEdit: async (req, res) => {
+        const id = req.params.id
+        console.log(id)
         try {
           let mongoose = require('mongoose')
           const contacts = await Contact.find();
           console.log(req.body)
-          res.render("edit.ejs", {  contacts: contacts, user: req.user, });
+          res.render("edit.ejs", {  contacts: contacts, user: req.user, idContact: id });
         } catch (err) {
           console.log(err); 
         }
