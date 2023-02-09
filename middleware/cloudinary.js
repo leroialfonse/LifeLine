f38aeb58989e68499b2d9cefd8a880a4d0e515c7
Brandon White <brandonkwhite3@gmail.com>
2023-02-09 16:34:58 -0500

    Change to middleware.

--- middleware/cloudinary.js
+++ middleware/cloudinary.js
@@ -1,6 +1,9 @@
-const cloudinary = require("cloudinary").v2;
-
+// reads in the cloudinary env variable - has to be first.
 require("dotenv").config({ path: "./config/.env" });
+// Aliasing version 2, and referencing with a variable.
+const cloudinary = require("cloudinary").v2;
+// Cloudinary picks up env and is now configured. 
+console.log(cloudinary.config().cloud_name);
 
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
