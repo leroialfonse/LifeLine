c5245e59191ae13aa3191ea7908813e1b896ae5a
Brandon White <brandonwhitedev@gmail.com>
2023-10-30 14:48:39 -0400

    Considering the timer again

--- controllers/home.js
+++ controllers/home.js
@@ -3,16 +3,18 @@ const Med = require("../models/Med");
 
 
 module.exports = {
-  getIndex: (req, res) => {
-    res.render("index.ejs" ,{user: req.user});  
+
+  getIndex: async (req, res) => {
+    const user = await User.find({})
+    res.render("index.ejs", { user: req.user });
   },
   getHome: async (req, res) => {
     try {
       let mongoose = require('mongoose')
-      const meds = await Med.find( { user: req.user });
-      res.render("home.ejs", {meds: meds, user: req.user});
+      const meds = await Med.find({ user: req.user });
+      res.render("home.ejs", { meds: meds, user: req.user });
     } catch (err) {
-      console.log(err); 
+      console.log(err);
     }
   },
 };
--- models/Contact.js
+++ models/Contact.js
@@ -8,7 +8,8 @@ const ContactSchema = new mongoose.Schema({
     type: String
   },
   phone: {
-    type: String
+    type: String,
+    match: /^(\()?\d{3}(\))?(-|\s)?\d{3}(-|\s)\d{4}$/
   },
   // user: {
   //   type: mongoose.Schema.Types.ObjectId,
@@ -17,6 +18,8 @@ const ContactSchema = new mongoose.Schema({
   userId: {
     type: String,
   },
+  // TODO: Link the meds to the doctors with Ref.
+  // perscriptions: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Med' }],
   createdAt: {
     type: Date,
     default: Date.now,
